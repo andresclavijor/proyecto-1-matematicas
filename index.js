60e7bf04b26f4b732885cbc10e78ad17f40470e1
Anartz Mugika <mugan86@gmail.com>
2018-11-30 17:57:44 +0100

    Añadir modulo principal del proyecto

---
+++ index.js
@@ -0,0 +1,42 @@
+module.exports = {
+    /**
+     * Suma números
+     * @example
+     * 1 + 1 = 2
+     * @param { number } n1 Número 1 a sumar
+     * @param { number } n2 Número 2 a sumar
+     */
+    suma: function(n1, n2) {
+        return n1 + n2;
+    },
+    /**
+     * Resta números
+     * @example
+     * 1 - 1 = 0
+     * @param { number } n1 Número 1 a restar
+     * @param { number } n2 Número 2 a restar
+     */
+    resta: function (n1, n2) {
+        return n1 - n2;
+    },
+    /**
+     * Multiplica números
+     * @example
+     * 1 / 1 = 1
+     * @param { number } n1 Número 1 a multiplicar
+     * @param { number } n2 Número 2 a multiplicar
+     */
+    multiplicacion: function(n1, n2) {
+        return n1 * n2;
+    },
+    /**
+     * Divide números
+     * @example
+     * 1 / 1 = 1
+     * @param { number } n1 Número 1 a dividir
+     * @param { number } n2 Número 2 a dividir 
+     */
+    division: function (n1, n2) {
+        return n1 / n2;
+    }
+}
